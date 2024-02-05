807a38f2bf61376fc3d53f894541a03b02101bdc
tapas20 <140873447+tapas20@users.noreply.github.com>
2024-02-05 21:19:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* === typing animation === */
+var typed = new Typed(".typing",{
+    strings:["","Programmer","Frontend Developer","Backend Developer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
