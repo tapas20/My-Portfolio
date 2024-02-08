28db40e1d2f1f2ff47ca5c5f83f559212dc4ea70
tapas20 <140873447+tapas20@users.noreply.github.com>
2024-02-08 14:41:49 +0530

    first

--- js/script.js
+++ js/script.js
@@ -90,21 +90,3 @@
                 allSection[i].classList.toggle("open");
             }
         }
-
-        /* Contact  */
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbzauOtQ6d5URPgOXKqEQW0sUwo9C4f6AMBOGppRL_D7vgpUK7KO0qLAyralrGTEkMSs5w/exec'
-        const form = document.forms['submit-to-google-sheet'];
-        const msg = document.getElementById("msg");
-      
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                msg.innerHTML = "Message sent successfully"
-                setTimeout(function(){
-                    msg.innerHTML = ""
-                }, 5000);
-                form.reset();
-            })
-            .catch(error => console.error('Error!', error.message));
-        });
