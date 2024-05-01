e2df8c32a300d20163cc84fb08b7f697ea667d51
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-05-01 14:56:14 +0530

    frontpage

--- js/script.js
+++ js/script.js
@@ -1,110 +1,104 @@
 /* === typing animation === */
-    var typed = new Typed(".typing",{
-    strings:["","Programmer","Frontend Developer","Backend Developer","Web Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Programmer",
+    "Web Developer",
+    "Student",
+    "Frontend Developer",
+    "Engineer",
+    "Backend Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 /* === Aside === */
-    const nav = document.querySelector(".nav"),
-    navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length,
-    allSection = document.querySelectorAll(".section"),
-    totalSection = allSection.length;
-    for(let i=0; i<totalNavList; i++)
-    {
-        const a = navList[i].querySelector("a");
-        a.addEventListener("click", function()
-        {
-            removeBackSection();
-            for(let j=0; j<totalNavList; j++) 
-            {
-                if(navList[j].querySelector("a").classList.contains("active"))
-                {
-                    addBackSection(j);
-                    // allSection[j].classList.add("back-section");
-                }
-                navList[j].querySelector("a").classList.remove("active");
-            }
-            this.classList.add("active");
-            showSection(this);
-            if(window.innerWidth < 1200)
-            {
-                asideSectionTogglerBtn();
-            }
-        });
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    removeBackSection();
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        addBackSection(j);
+        // allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
     }
-    function removeBackSection()
-    {
-        for(let i=0; i<totalSection; i++)
-        {
-            allSection[i].classList.remove("back-section");
-        }
+    this.classList.add("active");
+    showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
     }
-    function addBackSection(num)
-    {
-        allSection[num].classList.add("back-section");
+  });
+}
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section");
+  }
+}
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
+}
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
     }
-    function showSection(element)
-    {
-        for(let i=0; i<totalSection; i++)
-        {
-            allSection[i].classList.remove("active");
-        }
-        const target = element.getAttribute("href").split("#")[1];
-        document.querySelector("#" + target).classList.add("active")
-    }
-    function updateNav(element)
-    {
-        for(let i=0; i<totalNavList; i++)
-        {
-            navList[i].querySelector("a").classList.remove("active");
-            const target = element.getAttribute("href").split("#")[1];
-            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
-            {
-                navList[i].querySelector("a").classList.add("active");
-            }
-        }
-    }
-    document.querySelector(".hire-me").addEventListener("click", function()
-    {
-        const sectionIndex = this.getAttribute("data-section-index");
-        // console.log(sectionIndex);
-        showSection(this);
-        updateNav(this);
-        removeBackSection();
-        addBackSection(sectionIndex);
-    });
-    const navTogglerBtn = document.querySelector(".nav-toggler"),
-        aside = document.querySelector(".aside");
-        navTogglerBtn.addEventListener("click", () => 
-        {
-            asideSectionTogglerBtn();
-        });
-        function asideSectionTogglerBtn()
-        {
-            aside.classList.toggle("open");
-            navTogglerBtn.classList.toggle("open");
-            for(let i=0; i<totalSection; i++)
-            {
-                allSection[i].classList.toggle("open");
-            }
-        }
+  }
+}
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  // console.log(sectionIndex);
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
 
-        /* Contact  */
-        
-        const form = document.forms['submit-to-google-sheet'];
-        const msg = document.getElementById("msg");
-      
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                msg.innerHTML = "Message sent successfully"
-                setTimeout(function(){
-                    msg.innerHTML = ""
-                }, 5000);
-                form.reset();
-            })
-            .catch(error => console.error('Error!', error.message));
-        });
+/* Contact  */
+
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
